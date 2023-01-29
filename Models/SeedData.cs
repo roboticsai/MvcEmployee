ac35fb145f4a25033151479e78f82df4b0881c70
dell <dinesh99981@gmail.com>
2023-01-29 13:33:36 +0545

    models corrected

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -1,56 +1,62 @@
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.Extensions.DependencyInjection;
-// // using MvcEmployee.Data;
-// using System;
-// using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcEmployee.Data;
+using System;
+using System.Linq;
 
-// namespace MvcEmployee.Models;
+namespace MvcEmployee.Models;
 
-// public static class SeedData
-// {
-//     public static void Initialize(IServiceProvider serviceProvider)
-//     {
-//         using (var context = new MvcEmployeeContext(
-//             serviceProvider.GetRequiredService<
-//                 DbContextOptions<MvcEmployeeContext>>()))
-//         {
-//             // Look for any Employee.
-//             if (!context.Employee.Any())
-//             {
-//                 context.Employee.AddRange(
-//                 new Employee
-//                 {
-//                     Name = "Hari"
-//                 },
-//                 new Employee
-//                 {
-//                     Name = "Hari"
-//                 },
-//                 new Employee
-//                 {
-//                     Name = "Hari"
-//                 }
-//                 );
-//             }
-//             if (!context.QualificationList.Any())
-//             {
-//                 context.QualificationList.AddRange(
-//                 new QualificationList
-//                 {
-//                     Name = "SLC"
-//                 },
-//                 new QualificationList
-//                 {
-//                     Name = "BE"
-//                 },
-//                 new QualificationList
-//                 {
-//                     Name = "ME"
-//                 }
-//                 );
-//             }
-
-//         context.SaveChanges();
-//         }
-//     }
-// }
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcEmployeeContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcEmployeeContext>>()))
+        {
+            // Look for any Gender.
+            if (context.Employee.Any())
+            {
+                return;
+            }
+            context.Employee.AddRange(
+                new Employee
+                {
+                    Name = "Male"
+                },
+                new Employee
+                {
+                    Name = "Female"
+                },
+                new Employee
+                {
+                    Name = "Third Party"
+                }
+            );
+            // Look for any Employee.
+            if (context.QualificationList.Any())
+            {
+                return;
+            }     
+            context.QualificationList.AddRange(
+                new QualificationList
+                {
+                    Name = "SLC"
+                },
+                new QualificationList
+                {
+                    Name = "BE"
+                },
+                new QualificationList
+                {
+                    Name = "ME"
+                },
+                new QualificationList
+                {
+                    Name = "PHD"
+                }
+            );   
+            context.SaveChanges();     
+        }
+    }
+}
